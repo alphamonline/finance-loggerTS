5ac958389523aa3b1e76a32510c818e9f5ad9659
Alpha <alphamonline@gmail.com>
2023-08-03 20:25:46 +0200

    Rendering html

--- public/app.js
+++ public/app.js
@@ -1,27 +1,15 @@
 import { Invoice } from './classes/Invoice.js';
+import { ListTemplate } from './classes/ListTemplate.js';
 import { Payment } from './classes/Payment.js';
-// let docOne: HasFormatter;
-// let docTwo: HasFormatter;
-// docOne = new Invoice('kay', 'website', 300);
-// docTwo = new Payment('nail', 'website', 600);
-// let docs: HasFormatter[] = [];
-// docs.push(docOne);
-// docs.push(docTwo);
-// console.log(docs);
-// const invOne = new Invoice('tino', 'work on the tino website', 250);
-// const invTwo = new Invoice('aga', 'work on the aga website', 150);
-// let invoices: Invoice[] = [];
-// invoices.push(invOne);
-// invoices.push(invTwo);
-// invoices.forEach(inv => {
-//   console.log(inv.client, inv.amount, inv.format());
-// });
-//Dom and Type Casting
+// Dom and Type Casting
 const form = document.querySelector('.new-item-form');
 const type = document.querySelector('#type');
 const tofrom = document.querySelector('#tofrom');
 const details = document.querySelector('#details');
 const amount = document.querySelector('#amount');
+// list template instate
+const ul = document.querySelector('.item-list');
+const list = new ListTemplate(ul);
 form.addEventListener('submit', (e) => {
     e.preventDefault();
     let doc;
@@ -31,5 +19,5 @@ form.addEventListener('submit', (e) => {
     else {
         doc = new Payment(tofrom.value, details.value, amount.valueAsNumber);
     }
-    console.log(doc);
+    list.render(doc, type.value, 'end');
 });
