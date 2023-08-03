710b849642635e7e2a5433e2f7b9d85117c50f9c
Alpha <alphamonline@gmail.com>
2023-08-03 14:26:56 +0200

    classes, constructor, methods, arrays

--- public/app.js
+++ public/app.js
@@ -1,4 +1,20 @@
 "use strict";
+//classes
+class Invoice {
+    constructor(c, d, a) {
+        this.client = c;
+        this.details = d;
+        this.amount = a;
+    }
+    format() {
+        return '${this.client} owes ${this.amount} for ${this.details}';
+    }
+}
+const invOne = new Invoice('tino', 'work on the tino website', 250);
+const invTwo = new Invoice('aga', 'work on the aga website', 150);
+let invoices = [];
+invoices.push(invOne, invTwo);
+console.log(invoices);
 //Dom and Type Casting
 const form = document.querySelector('.new-item-form');
 const type = document.querySelector('#type');
--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,29 @@
+//classes
+class Invoice {
+  client: string;
+  details: string;
+  amount: number;
+
+  constructor(c: string, d: string, a: number){
+    this.client = c;
+    this.details = d;
+    this.amount = a;
+  }
+
+  format() {
+    return '${this.client} owes ${this.amount} for ${this.details}'
+  }
+}
+
+const invOne = new Invoice('tino', 'work on the tino website', 250);
+const invTwo = new Invoice('aga', 'work on the aga website', 150);
+
+let invoices: Invoice[] = [];
+invoices.push(invOne, invTwo);
+
+console.log(invoices);
+
+
 //Dom and Type Casting
 const form = document.querySelector('.new-item-form') as HTMLFormElement;
 const type = document.querySelector('#type') as HTMLSelectElement;
